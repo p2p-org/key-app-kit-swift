d2006172af50f3309ca513f40c525021cd2690f8
Tran Giang Long <trangianglong4003@gmail.com>
2022-07-26 14:49:05 +0200

    feat(restore-wallet): add state machine

---
+++ Sources/Onboarding/RestoreWalletStateMachine.swift
@@ -0,0 +1,41 @@
+// Copyright 2022 P2P Validator Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+import Foundation
+
+public typealias RestoreWalletStateMachine = StateMachine<RestoreWalletState>
+
+public enum RestoreWalletState: Codable, State, Equatable {
+    public typealias Event = RestoreWalletEvent
+    public typealias Provider = TKeyFacade
+    public static var initialState: RestoreWalletState = .signIn
+    
+    public func accept(
+        currentState: RestoreWalletState,
+        event: RestoreWalletEvent,
+        provider: Provider
+    ) async throws -> RestoreWalletState {
+        switch currentState {
+        case .signIn:
+            switch event {
+            case let .signInWithDeviceShare(tokenID, deviceShare):
+                let result = try await provider.signIn(tokenID: .init(value: tokenID, provider: "apple"), deviceShare: deviceShare)
+                return .restoredData(solPrivateKey: result.privateSOL, ethPublicKey: result.reconstructedETH)
+            case let .signInWithCustomShare(tokenID):
+                let result = try await provider.signIn(tokenID: .init(value: tokenID, provider: "apple"), withCustomShare: "")
+                return .restoredData(solPrivateKey: result.privateSOL, ethPublicKey: result.reconstructedETH)
+            }
+        case .restoredData:
+            return currentState
+        }
+    }
+    
+    case signIn
+    case restoredData(solPrivateKey: String, ethPublicKey: String)
+}
+
+public enum RestoreWalletEvent {
+    case signInWithDeviceShare(tokenID: String, deviceShare: String)
+    case signInWithCustomShare(tokenID: String)
+}
