6cde763bf53ca4c0c4d57dd8e2e5422b6e76635a
Tran Giang Long <trangianglong4003@gmail.com>
2022-09-14 01:03:21 +0200

    feat(solend): service

--- Sources/Solend/SolendService.swift
+++ Sources/Solend/SolendService.swift
@@ -22,16 +22,17 @@ class SolendServiceImpl: SolendService {
     private var owner: Account
     private var lendingMark: String
 
-    let availableSymbols: CurrentValueSubject<[SolendSymbol]> = .init(["SOL", "USDT", "USDC", "BTC", "ETH"])
-    let deposits: CurrentValueSubject<[SolendUserDeposit]> = .init([])
-    let marketInfo: CurrentValueSubject<[SolendMarketInfo]> = .init([])
+    let availableSymbols: CurrentValueSubject<[SolendSymbol], Never> = .init(["SOL", "USDT", "USDC", "BTC", "ETH"])
+    let deposits: CurrentValueSubject<[SolendUserDeposit], Never> = .init([])
+    let marketInfo: CurrentValueSubject<[SolendMarketInfo], Never> = .init([])
 
-    init(owner: Account) {
+    init(owner: Account, lendingMark: String) {
         self.owner = owner
+        self.lendingMark = lendingMark
     }
 
     var hasDeposits: Bool {
-        deposits.value.first { Double($0.depositedAmount) > 0 } != nil
+        deposits.value.first { (Double($0.depositedAmount) ?? 0) > 0 } != nil
     }
 
     func getCollateralAccounts(rpcURL: String, owner: String) async throws -> [SolendCollateralAccount] {
@@ -39,23 +40,24 @@ class SolendServiceImpl: SolendService {
     }
 
     func updateStatus() async throws {
-        async let marketInfo: [SolendMarketInfo] = try solend
-            .getMarketInfo(symbols: availableSymbols.value, pool: "main")
-            .map { token, marketInfo -> SolendMarketInfo in .init(
-                symbol: token,
-                currentSupply: marketInfo.currentSupply,
-                depositLimit: marketInfo.currentSupply,
-                supplyInterest: marketInfo.supplyInterest
-            ) }
-
-        async let deposits: [SolendUserDeposit] = try await solend.getUserDeposits(
-            owner: owner.publicKey.base58EncodedString,
-            poolAddress: lendingMark
+        try await updateValues(
+            marketInfo: solend
+                .getMarketInfo(symbols: availableSymbols.value, pool: "main")
+                .map { token, marketInfo -> SolendMarketInfo in .init(
+                    symbol: token,
+                    currentSupply: marketInfo.currentSupply,
+                    depositLimit: marketInfo.currentSupply,
+                    supplyInterest: marketInfo.supplyInterest
+                ) },
+            deposits: solend.getUserDeposits(
+                owner: owner.publicKey.base58EncodedString,
+                poolAddress: lendingMark
+            )
         )
-
-        try await {
-            try self.deposits.send(deposits)
-            try self.marketInfo.send(marketInfo)
-        }
+    }
+    
+    func updateValues(marketInfo: [SolendMarketInfo], deposits: [SolendUserDeposit]) {
+        self.deposits.send(deposits)
+        self.marketInfo.send(marketInfo)
     }
 }
