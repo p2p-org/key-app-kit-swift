f1dfbf84d6c779cbf4c5a04dd1370cd6c3f55dbd
Alexey Sidorov <ody344@gmail.com>
2022-09-14 13:02:49 +0300

    New countries file

--- Sources/CountriesAPI/CountriesAPI.swift
+++ Sources/CountriesAPI/CountriesAPI.swift
@@ -6,7 +6,7 @@ public protocol CountriesAPI {
 
 public final class CountriesAPIImpl: CountriesAPI {
     public init() {}
-    
+
     public func fetchCountries() async throws -> Countries {
         try await Task {
             let b: Bundle
@@ -15,10 +15,12 @@ public final class CountriesAPIImpl: CountriesAPI {
             #else
             b = Bundle(for: Self.self)
             #endif
-            let url = b.url(forResource: "countries", withExtension: "json")!
+            let url = b.url(forResource: "countries_2", withExtension: "json")!
             try Task.checkCancellation()
             let data = try Data(contentsOf: url)
             let countries = try JSONDecoder().decode(Countries.self, from: data)
+                .filter { !$0.dialCode.isEmpty }
+                .filter { $0.status == .assigned || $0.status == .userAssigned }
             try Task.checkCancellation()
             return countries
         }.value
--- Sources/CountriesAPI/Models.swift
+++ Sources/CountriesAPI/Models.swift
@@ -2,20 +2,44 @@ import Foundation
 
 // MARK: - Country
 public struct Country: Codable, Hashable {
-    public let name, dialCode, code: String
+    public let name: String
+    public let code: String
+    public let countryCallingCodes: [String]
     public let emoji: String?
+    public var dialCode: String
+    var status: Status?
 
     enum CodingKeys: String, CodingKey {
         case name
-        case dialCode = "dial_code"
-        case code, emoji
+        case code = "alpha2"
+        case countryCallingCodes
+        case emoji
+        case status
     }
 
     public init(name: String, dialCode: String, code: String, emoji: String?) {
         self.name = name
-        self.dialCode = dialCode
         self.code = code
+        self.dialCode = dialCode
         self.emoji = emoji
+        self.countryCallingCodes = [dialCode]
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.code = try container.decode(String.self, forKey: .code)
+        self.countryCallingCodes = try container.decode([String].self, forKey: .countryCallingCodes)
+        self.emoji = try? container.decode(String.self, forKey: .emoji)
+        self.status = try? container.decode(Status.self, forKey: .status)
+        self.dialCode = self.countryCallingCodes.first ?? ""
+    }
+
+    enum Status: String, Codable {
+        case assigned
+        case reserved
+        case deleted
+        case userAssigned = "user assigned"
     }
 }
 
