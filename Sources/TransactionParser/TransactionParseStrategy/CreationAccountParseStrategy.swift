3562801b96da25470d7c1fcd9ed56767d73446d3
Tran Giang Long <trangianglong4003@gmail.com>
2022-10-26 18:36:10 +0700

    fix(transaction-parser): create account transaction

--- Sources/TransactionParser/TransactionParseStrategy/CreationAccountParseStrategy.swift
+++ Sources/TransactionParser/TransactionParseStrategy/CreationAccountParseStrategy.swift
@@ -7,44 +7,57 @@ import SolanaSwift
 
 /// The strategy for parsing creation account transactions.
 public class CreationAccountParseStrategy: TransactionParseStrategy {
-  private let tokensRepository: SolanaTokensRepository
+    private let tokensRepository: SolanaTokensRepository
 
-  init(tokensRepository: SolanaTokensRepository) { self.tokensRepository = tokensRepository }
-  
-  public func isHandlable(with transactionInfo: SolanaSwift.TransactionInfo) -> Bool {
-    let instructions = transactionInfo.transaction.message.instructions
-    switch instructions.count {
-    case 1: return instructions[0].program == "spl-associated-token-account"
-    case 2: return instructions.first?.parsed?.type == "createAccount" && instructions.last?.parsed?
-      .type == "initializeAccount"
-    default: return false
+    init(tokensRepository: SolanaTokensRepository) { self.tokensRepository = tokensRepository }
+
+    public func isHandlable(with transactionInfo: SolanaSwift.TransactionInfo) -> Bool {
+        let instructions = transactionInfo.transaction.message.instructions
+        switch instructions.count {
+        case 1:
+            return instructions[0].program == "spl-associated-token-account" || instructions.first?.parsed?
+                .type == "create"
+        case 2:
+            if instructions.first?.parsed?.type == "create" {
+                return true
+            } else if instructions.first?.parsed?.type == "createAccount",
+                      instructions.last?.parsed?.type == "initializeAccount"
+            {
+                return true
+            }
+            return false
+
+        default: return false
+        }
     }
-  }
-  
-  public func parse(_ transactionInfo: SolanaSwift.TransactionInfo, config _: Configuration) async throws -> AnyHashable? {
-    let instructions = transactionInfo.transaction.message.instructions
 
-    if let program = extractProgram(instructions, with: "spl-associated-token-account") {
-      let token = try await tokensRepository.getTokenWithMint(program.parsed?.info.mint)
-      return CreateAccountInfo(fee: nil, newWallet: Wallet(pubkey: program.parsed?.info.account, token: token))
-    } else {
-      let info = instructions[0].parsed?.info
-      let initializeAccountInfo = instructions.last?.parsed?.info
-      let fee = info?.lamports?.convertToBalance(decimals: Decimals.SOL)
+    public func parse(
+        _ transactionInfo: SolanaSwift.TransactionInfo,
+        config _: Configuration
+    ) async throws -> AnyHashable? {
+        let instructions = transactionInfo.transaction.message.instructions
 
-      let token = try await tokensRepository.getTokenWithMint(initializeAccountInfo?.mint)
-      return CreateAccountInfo(
-        fee: fee,
-        newWallet: Wallet(
-          pubkey: info?.newAccount,
-          lamports: nil,
-          token: token
-        )
-      )
+        if let program = extractProgram(instructions, with: "spl-associated-token-account") {
+            let token = try await tokensRepository.getTokenWithMint(program.parsed?.info.mint)
+            return CreateAccountInfo(fee: nil, newWallet: Wallet(pubkey: program.parsed?.info.account, token: token))
+        } else {
+            let info = instructions[0].parsed?.info
+            let initializeAccountInfo = instructions.last?.parsed?.info
+            let fee = info?.lamports?.convertToBalance(decimals: Decimals.SOL)
+
+            let token = try await tokensRepository.getTokenWithMint(initializeAccountInfo?.mint)
+            return CreateAccountInfo(
+                fee: fee,
+                newWallet: Wallet(
+                    pubkey: info?.newAccount,
+                    lamports: nil,
+                    token: token
+                )
+            )
+        }
     }
-  }
 
-  func extractProgram(_ instructions: [ParsedInstruction], with name: String) -> ParsedInstruction? {
-    instructions.first { $0.program == name }
-  }
+    func extractProgram(_ instructions: [ParsedInstruction], with name: String) -> ParsedInstruction? {
+        instructions.first { $0.program == name }
+    }
 }
