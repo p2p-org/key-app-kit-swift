02bfc081ca0bc4b9e4d490279b3afd9b6c06a0df
Elizaveta Semenova <thesharkight84@gmail.com>
2022-11-30 19:56:58 +0600

    [PWN-6023] Implementation of SendChooseFeeService

--- Sources/Send/Input/Services/SendFeeCalculator.swift
+++ Sources/Send/Input/Services/SendFeeCalculator.swift
@@ -175,17 +175,3 @@ public enum SendFeeCalculatorError: String, Swift.Error, LocalizedError {
         // swiftlint:enable swiftgen_strings
     }
 }
-
-extension Sequence {
-    func asyncMap<T>(
-        _ transform: (Element) async throws -> T
-    ) async rethrows -> [T] {
-        var values = [T]()
-
-        for element in self {
-            try await values.append(transform(element))
-        }
-
-        return values
-    }
-}
