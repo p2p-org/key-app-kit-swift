bd337d13e033f63ccba0b65438a1041ba9981651
Chung Tran <bigearsenal@gmail.com>
2022-12-23 15:21:43 +0700

    refactor: Models

--- Sources/Sell/ActionService/SellActionService.swift
+++ Sources/Sell/ActionService/SellActionService.swift
@@ -1,16 +1,5 @@
 import Foundation
 
-public enum SellActionServiceError: Error {
-    case invalidURL
-}
-
-public protocol SellActionServiceQuote {
-    var extraFeeAmount: Double { get }
-    var feeAmount: Double { get }
-    var baseCurrencyPrice: Double { get }
-    var quoteCurrencyAmount: Double { get }
-}
-
 public protocol SellActionService {
     associatedtype Provider: SellActionServiceProvider
 
