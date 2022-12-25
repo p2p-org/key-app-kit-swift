92afe9a9274147508de8c064081a8b333aeda8a5
Chung Tran <bigearsenal@gmail.com>
2022-12-25 18:04:18 +0700

    feat: SellTransactionsRepositoryImpl

--- Sources/Sell/DataService/TransactionsRepository/SellTransactionsRepository.swift
+++ Sources/Sell/DataService/TransactionsRepository/SellTransactionsRepository.swift
@@ -23,20 +23,44 @@ public protocol SellTransactionsRepository: Actor {
 public actor SellTransactionsRepositoryImpl: SellTransactionsRepository {
     
     // MARK: - Properties
+
     /// Transactions subject
     @Published public var transactions: [SellDataServiceTransaction] = []
     
+    /// Key for storing deletedTransactionIds in UserDefaults
+    private static let deletedTransactionIdsKey = "SellTransactionsRepository.deletedTransactionIds"
+    
+    /// Deleted transactions id
+    private var deletedTransactionIds: [String] {
+        didSet {
+            guard let data = try? JSONEncoder().encode(deletedTransactionIds) else {
+                return
+            }
+            UserDefaults.standard.set(data, forKey: Self.deletedTransactionIdsKey)
+        }
+    }
+    
     // MARK: - Initializer
-    public init() {}
+    public init() {
+        // retrieve deleted transaction ids
+        if let data = UserDefaults.standard.data(forKey: Self.deletedTransactionIdsKey),
+           let array = try? JSONDecoder().decode([String].self, from: data)
+        {
+            deletedTransactionIds = array
+        } else {
+            deletedTransactionIds = []
+        }
+    }
     
     // MARK: - Methods
     /// Set transactions
     public func setTransactions(_ transactions: [SellDataServiceTransaction]) {
-        self.transactions = transactions
+        self.transactions = transactions.filter { !deletedTransactionIds.contains($0.id) }
     }
     
     /// Delete transaction
     public func deleteTransaction(id: String) {
         transactions.removeAll(where: {$0.id == id})
+        deletedTransactionIds.append(id)
     }
 }
