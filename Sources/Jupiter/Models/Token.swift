73ce349deaf1f92f8d3849c95702c3a08ede4971
Elizaveta Semenova <thesharkight84@gmail.com>
2023-02-10 15:13:56 +0300

    [PWN-7098] Add ability to encode Jupiter.Token and manually initialise it

--- Sources/Jupiter/Models/Token.swift
+++ Sources/Jupiter/Models/Token.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct Token: Decodable {
+public struct Token: Codable {
     public let address: String
     public let chainId: Int
     public let decimals: Int
@@ -16,8 +16,19 @@ public struct Token: Decodable {
     public let logoURI: String?
     public let extensions: Extensions?
     public let tags: [String]
-    
-    public struct Extensions: Decodable {
+
+    public struct Extensions: Codable {
         public let coingeckoId: String?
+    }
+
+    public init(address: String, chainId: Int, decimals: Int, name: String, symbol: String, logoURI: String?, extensions: Extensions?, tags: [String]) {
+        self.address = address
+        self.chainId = chainId
+        self.decimals = decimals
+        self.name = name
+        self.symbol = symbol
+        self.logoURI = logoURI
+        self.extensions = extensions
+        self.tags = tags
     }
 }
